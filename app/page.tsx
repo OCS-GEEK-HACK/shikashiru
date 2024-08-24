f26661d15e3f556317b46448e3185c92cb95c35e
yuto ichinose <yuto.0526.sano@gmail.com>
2024-08-25 00:31:00 +0900

    feat: ヘッダーコンポーネント作成

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,14 @@
+import { Header } from "@/components/header";
 import { MapComponent } from "@/components/map";
 import { MapProvider } from "@/components/map-provider";
-import { Heading, HStack, SegmentedControl, SegmentedControlButton, VStack, Text } from "@yamada-ui/react";
+import { VStack, } from "@yamada-ui/react";
 
 export default function Home() {
   return (
     <VStack w="full" h="100dvh" gap={0}>
-      <HStack w="full" as="header" justifyContent="space-between" background="headerAlpha.600" backdropBlur="blur(10px)" backdropFilter="blur(10px)" backdropSaturate="saturate(180%)" boxShadow="sm" p="md" position="fixed" top={0} zIndex={1}>
-        <Heading fontFamily="FUTENE" color="#60694B">シカシる<Text as="span" fontSize="md">なのだ。</Text></Heading>
-        <SegmentedControl>
-          <SegmentedControlButton value="list">一覧</SegmentedControlButton>
-          <SegmentedControlButton value="category-filter">カテゴリー・フィルター</SegmentedControlButton>
-        </SegmentedControl>
-      </HStack>
+      <Header />
       <MapProvider>
-        <MapComponent/>
+        <MapComponent />
       </MapProvider>
     </VStack>
   );
---
+++ components/header.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { Button, ButtonGroup, Heading, HStack, Text } from "@yamada-ui/react";
+import { FC, useState } from "react";
+
+const buttonsData = [
+    {
+        key: "list",
+        value: "一覧"
+    },
+    {
+        key: "category-filter",
+        value: "カテゴリー・フィルター"
+    }
+]
+
+export const Header:FC = () => {
+    const [active, setActive] = useState<number | undefined>(undefined)
+    return <HStack w="full" as="header" justifyContent="space-between" background="headerAlpha.600" backdropBlur="blur(10px)" backdropFilter="blur(10px)" backdropSaturate="saturate(180%)" boxShadow="sm" p="md" position="fixed" top={0} zIndex={1}>
+    <Heading fontFamily="FUTENE" color="#60694B">シカシる<Text as="span" fontSize="md">なのだ。</Text></Heading>
+    <ButtonGroup isAttached>
+        {
+            buttonsData.map((button, index) => (
+                <Button
+                    key={button.key}
+                    variant={active === index ? "solid" : "ghost"}
+                    onClick={() => {
+                        setActive(active === index ? undefined : index)
+                    }}
+                >{button.value}</Button>
+            ))
+        }
+    </ButtonGroup>
+  </HStack>
+}
