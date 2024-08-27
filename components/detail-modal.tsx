f0a91685ee058c5b26a6d716fe405ab0c7183fd0
038bkn <038bkn@gmail.com>
2024-08-27 23:27:31 +0900

    feat: 詳細モーダルにカルーセル追加

--- components/detail-modal.tsx
+++ components/detail-modal.tsx
@@ -1,7 +1,16 @@
 "use client";
+
+import {
+  Carousel,
+  CarouselSlide,
+  CarouselControlNext,
+  CarouselControlPrev,
+  CarouselIndicators,
+} from "@yamada-ui/carousel";
 import { Volume2Icon } from "@yamada-ui/lucide";
 import {
   Button,
+  Center,
   Heading,
   HStack,
   IconButton,
@@ -26,17 +35,60 @@ export const DetailModal: FC<DetailModalProps> = ({
   isOpen,
   name,
   description,
+  images,
   onNavigate,
   onClose,
 }) => {
   return (
-    <Modal isOpen={isOpen} onClose={onClose}>
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      maxWidth="1200px"
+      fontFamily="FUTENE"
+      size="lg"
+    >
       <ModalHeader></ModalHeader>
 
       <ModalBody py="md">
-        {/* カルーセルを置く */}
+        {/* カルーセル */}
+        <Carousel slideSize="50%" autoplay delay={3000} dragFree>
+          {images.map((image, index) => (
+            <CarouselSlide
+              key={index}
+              as={Center}
+              bg="primary"
+              style={{
+                height: "300px",
+              }}
+            >
+              <img
+                src={image}
+                alt={`${name} image ${index + 1}`}
+                style={{
+                  width: "100%",
+                  height: "100%",
+                  objectFit: "cover",
+                }}
+              />
+            </CarouselSlide>
+          ))}
+          <CarouselControlPrev
+            style={{
+              position: "absolute",
+              top: "40%",
+            }}
+          />
+          <CarouselControlNext
+            style={{
+              position: "absolute",
+              top: "40%",
+            }}
+          />
+          <CarouselIndicators />
+        </Carousel>
+
         <HStack justifyContent="space-between" w="full">
-          <Heading as="h2" size="md">
+          <Heading as="h2" size="2xl" fontFamily={"FUTENE"}>
             {name}
           </Heading>
           <IconButton
@@ -45,7 +97,9 @@ export const DetailModal: FC<DetailModalProps> = ({
             variant="outline"
           />
         </HStack>
-        <Text>{description}</Text>
+        <Text fontSize="xl" mt="2">
+          {description}
+        </Text>
       </ModalBody>
 
       <ModalFooter>
